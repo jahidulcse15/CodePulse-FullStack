cd6df5691c1d8fa558106608cf2c4d0a32f4659d
jakir <jahidulcse15@gmail.com>
2026-09-15 12:18:39 +0600

    Initial commit

---
+++ API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options) : base(Options) 
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
